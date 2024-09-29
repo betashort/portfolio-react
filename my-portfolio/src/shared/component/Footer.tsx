a3abd6b49624023575316f11d023c43b1a1a1d39
betashort <wshota.betashort@gmail.com>
2024-09-29 15:06:23 +0900

    deploy

--- my-portfolio/src/shared/component/Footer.tsx
+++ my-portfolio/src/shared/component/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className="w-full h-10 bg-gray-900 flex justify-center items-center ">
+      <div className="w-full h-10 mt-4 bg-gray-900 flex justify-center items-center ">
         <div className=" text-sm font-bold text-gray-200">©βshort</div>
       </div>
     </>
